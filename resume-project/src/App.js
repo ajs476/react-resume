3a9ec1ceb4579b3ace28efce371690e6af543afa
Alexander Sears <alexander.sears@moove-it.com>
2018-06-06 16:52:52 -0500

    created Career and Experience react components to use

--- resume-project/src/App.js
+++ resume-project/src/App.js
@@ -1,61 +1,84 @@
 import React from 'react';
 
+const CareerComponent = () => {
+  return(
+  <div>
+    <div className="heading">
+      <h2>
+        <i className="fas fa-user-circle"></i>Career Profile</h2>
+    </div>
+    <section className="content">
+      <h2 hidden="hidden">Career Information</h2>
+      <p>
+        After graduating from Northern Arizona University with a Bachelors
+        in Applied Computer Science I moved to Austin, Texas looking to begin
+        my career in software development. While searching for work I spent
+        a few months developing the mobile game Pazaak Cantina. I then was
+        accepted into the apprenticeship program at the software development
+        company Moove-it, located in downtown Austin. I am hopeful that I will
+        begin working as a full-time developer with Moove-it after finishing the apprenticeship.
+      </p>
+    </section>
+  </div>
+  );
+}
+
+const ExperienceComponent = (props) => {
+  return (
+    <div>
+      <section className="experience-heading">
+        <h2 hidden="hidden">Experience Information</h2>
+        <div className="exp-position-location">
+          <h3>{props.title}</h3>
+          <h4>{props.company}, {props.city}</h4>
+        </div>
+        <div className="exp-date">
+          <h4>{props.startDate} - {props.endDate}</h4>
+        </div>
+      </section>
+      <section className="content">
+        <h2 hidden="hidden">{props.title} Experience Information</h2>
+        <p>
+          {props.description}
+        </p>
+      </section>
+    </div>
+  );
+}
 
 const App = () => (
   <div>
     <section className="container">
       <section className="left-side">
-        <div className="heading">
-          <h2>
-            <i className="fas fa-user-circle"></i>Career Profile</h2>
-        </div>
-        <section className="content">
-          <h2 hidden="hidden">Career Information</h2>
-          <p>
-            After graduating from Northern Arizona University with a Bachelors
-            in Applied Computer Science I moved to Austin, Texas looking to begin
-            my career in software development. While searching for work I spent
-            a few months developing the mobile game Pazaak Cantina. I then was
-            accepted into the apprenticeship program at the software development
-            company Moove-it, located in downtown Austin. I am hopeful that I will
-            begin working as a full-time developer with Moove-it after finishing the apprenticeship.
-          </p>
-        </section>
+        <CareerComponent></CareerComponent>
         <div className="heading">
           <h2>
             <i className="fas fa-check-circle"></i>Experience</h2>
         </div>
-        <section className="experience-heading">
-          <h2 hidden="hidden">Work Experience Information</h2>
-          <div className="exp-position-location">
-            <h3>Apprentice Web Developer</h3>
-            <h4>Moove-it, Austin</h4>
-          </div>
-          <div className="exp-date">
-            <h4>2018 - Present</h4>
-          </div>
-        </section>
-        <section className="content">
-          <h2 hidden="hidden">Apprentice Experience Information</h2>
-          <p>
-            As an apprentice developer at Moove-it I am constantly learning new things. While currently focusing on the basics of web development using HTML and CSS, I will soon be gaining experience with modern front-end frameworks. Another the apprentices at Moove-it are doing is getting a ton of hands-on experience with Git. Although there are some struggles with git, I am continuing to get better and better using it each day.
-          </p>
-        </section>
-        <section className="experience-heading">
-          <div className="exp-position-location">
-            <h3>Android Developer</h3>
-            <h4>Freelance, Austin</h4>
-          </div>
-          <div className="exp-date">
-            <h4>2015 - 2018</h4>
-          </div>
-        </section>
-        <section className="content">
-          <h2 hidden="hidden">Android Developer Experience Information</h2>
-          <p>
-            I developed the android-based mobile game Pazaak Cantina - The Card Game in March 2018. After publishing the game on the Google Play Store, I have received hundreds of reviews and over a thousand downloads. The overall rating of Pazaak Cantina is a 4.6/5, and I couldnt be happier with the results. I am still working on small developments for the game, but overall I consider the application to be finished.
-          </p>
-        </section>
+        <ExperienceComponent
+          title="Apprentice Web Developer"
+          company="Moove-it"
+          city="Austin"
+          startDate="2018"
+          endDate="Present"
+          description="This is the description of the position. What an amazing example.
+          This is the description of the position. What an amazing example.
+          This is the description of the position. What an amazing example.
+          This is the description of the position. What an amazing example."
+          >
+        </ExperienceComponent>
+        <ExperienceComponent
+          title="Android Developer"
+          company="Freelance"
+          city="Phoenix"
+          startDate="2017"
+          endDate="2018"
+          description="This is the description of the position. What an amazing example.
+          This is the description of the position. What an amazing example.
+          This is the description of the position. What an amazing example.
+          This is the description of the position. What an amazing example."
+          >
+        </ExperienceComponent>
       </section>
       <section className="right-side">
         <h2 hidden="hidden">Personal Information</h2>
