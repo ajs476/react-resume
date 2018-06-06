3000a7217666633e67e1afcf1fa49962b6dc16eb
Alexander Sears <alexander.sears@moove-it.com>
2018-06-06 13:47:14 -0500

    added react to project

--- resume-project/webpack.config.js
+++ resume-project/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, './src/index.js'),
@@ -16,5 +17,16 @@ module.exports = {
         exclude: /node_modules/
       },
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './index.html')
+    })
+  ],
+  devServer: {
+    historyApiFallback: { index: '/' },
+      headers: {
+          'Access-Control-Allow-Origin': '*'
+      }
   }
 }
