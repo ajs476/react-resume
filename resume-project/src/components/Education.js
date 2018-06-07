c1c12aaab6119ae9e8420d0c525c3fa2451a3e28
Alexander Sears <alexander.sears@moove-it.com>
2018-06-07 10:27:06 -0500

    created Education component for RightSide

---
+++ resume-project/src/components/Education.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Education = () => (
+  <section className="education-info">
+    <header>Education</header>
+    <h3>BSc in Applied Computer Science</h3>
+    <h4>Northern Arizona University</h4>
+    <h4>2012-2017</h4>
+    <h3>Associates of Arts</h3>
+    <h4>Glendale Community College</h4>
+    <h4>2010-2012</h4>
+  </section>
+);
+
+export {Education};
--- resume-project/src/components/RightSide.js
+++ resume-project/src/components/RightSide.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import {Personal} from './Personal.js';
 import {Contact} from './Contact.js';
+import {Education} from './Education.js';
 
 const RightSide = () => (
-    <section className="right-side">
+  <section className="right-side">
       <Personal />
       <Contact />
-      <section className="education-info">
-        <header>Education</header>
-        <h3>BSc in Applied Computer Science</h3>
-        <h4>Northern Arizona University</h4>
-        <h4>2012-2017</h4>
-        <h3>Associates of Arts</h3>
-        <h4>Glendale Community College</h4>
-        <h4>2010-2012</h4>
-      </section>
+      <Education />
   </section>
 );
 
