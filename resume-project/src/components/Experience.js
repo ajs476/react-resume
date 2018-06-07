17362bb9fd7271f1c281959ca53707a23ee10031
Alexander Sears <alexander.sears@moove-it.com>
2018-06-07 15:15:10 -0500

    moved experience heading to experience component

--- resume-project/src/components/Experience.js
+++ resume-project/src/components/Experience.js
@@ -4,19 +4,24 @@ import experienceData from '../data/experience-data.json';
 
 const Experience = () => (
   <div>
-  {
-    experienceData.map((experienceObject, i) =>
-    <ExperienceItem
-      title={experienceObject.title}
-      company={experienceObject.company}
-      city={experienceObject.city}
-      startDate={experienceObject.startDate}
-      endDate={experienceObject.endDate}
-      description={experienceObject.description}
-      key={i}
-      >
-    </ExperienceItem>)
-  }
+    <div className="heading">
+      <h2>
+        <i className="fas fa-check-circle"></i>Experience
+      </h2>
+    </div>
+    {
+      experienceData.map((experienceObject, i) =>
+      <ExperienceItem
+        title={experienceObject.title}
+        company={experienceObject.company}
+        city={experienceObject.city}
+        startDate={experienceObject.startDate}
+        endDate={experienceObject.endDate}
+        description={experienceObject.description}
+        key={i}
+        >
+      </ExperienceItem>)
+    }
   </div>
 );
 
--- resume-project/src/components/LeftSide.js
+++ resume-project/src/components/LeftSide.js
@@ -5,11 +5,6 @@ import Experience from './Experience.js';
 const LeftSide = () => (
   <section className="left-side">
     <Career />
-    <div className="heading">
-      <h2>
-        <i className="fas fa-check-circle"></i>Experience
-      </h2>
-    </div>
     <Experience />
   </section>
 );
