e598b3389a5afdffc6d7b53ba304a1e4733a156d
Alexander Sears <alexander.sears@moove-it.com>
2018-06-07 09:41:56 -0500

    moved components into component folder, fixed component names

--- resume-project/src/App.js
+++ resume-project/src/App.js
@@ -1,84 +1,39 @@
 import React from 'react';
+import {Career} from './components/Career.js';
+import {Experience} from './components/Experience.js';
 
-const CareerComponent = () => {
-  return(
-  <div>
-    <div className="heading">
-      <h2>
-        <i className="fas fa-user-circle"></i>Career Profile</h2>
-    </div>
-    <section className="content">
-      <h2 hidden="hidden">Career Information</h2>
-      <p>
-        After graduating from Northern Arizona University with a Bachelors
-        in Applied Computer Science I moved to Austin, Texas looking to begin
-        my career in software development. While searching for work I spent
-        a few months developing the mobile game Pazaak Cantina. I then was
-        accepted into the apprenticeship program at the software development
-        company Moove-it, located in downtown Austin. I am hopeful that I will
-        begin working as a full-time developer with Moove-it after finishing the apprenticeship.
-      </p>
-    </section>
-  </div>
-  );
-}
-
-const ExperienceComponent = (props) => {
-  return (
-    <div>
-      <section className="experience-heading">
-        <h2 hidden="hidden">Experience Information</h2>
-        <div className="exp-position-location">
-          <h3>{props.title}</h3>
-          <h4>{props.company}, {props.city}</h4>
-        </div>
-        <div className="exp-date">
-          <h4>{props.startDate} - {props.endDate}</h4>
-        </div>
-      </section>
-      <section className="content">
-        <h2 hidden="hidden">{props.title} Experience Information</h2>
-        <p>
-          {props.description}
-        </p>
-      </section>
-    </div>
-  );
-}
 
 const App = () => (
   <div>
     <section className="container">
       <section className="left-side">
-        <CareerComponent></CareerComponent>
+        <Career />
         <div className="heading">
           <h2>
             <i className="fas fa-check-circle"></i>Experience</h2>
         </div>
-        <ExperienceComponent
+        <Experience
           title="Apprentice Web Developer"
           company="Moove-it"
           city="Austin"
           startDate="2018"
           endDate="Present"
-          description="This is the description of the position. What an amazing example.
-          This is the description of the position. What an amazing example.
-          This is the description of the position. What an amazing example.
-          This is the description of the position. What an amazing example."
+          description= {
+            'This is the description of the position. What an amazing example. This is the description of the position. What an amazing example. This is the description of the position. What an amazing example. This is the description of the position. What an amazing example.'
+          }
           >
-        </ExperienceComponent>
-        <ExperienceComponent
+        </Experience>
+        <Experience
           title="Android Developer"
           company="Freelance"
           city="Phoenix"
           startDate="2017"
           endDate="2018"
-          description="This is the description of the position. What an amazing example.
-          This is the description of the position. What an amazing example.
-          This is the description of the position. What an amazing example.
-          This is the description of the position. What an amazing example."
+          description= {
+            'This is the description of the position. What an amazing example. This is the description of the position. What an amazing example. This is the description of the position. What an amazing example. This is the description of the position. What an amazing example.'
+          }
           >
-        </ExperienceComponent>
+        </Experience>
       </section>
       <section className="right-side">
         <h2 hidden="hidden">Personal Information</h2>
---
+++ resume-project/src/components/Experience.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Experience = (props) => (
+    <div>
+      <section className="experience-heading">
+        <h2 hidden="hidden">Experience Information</h2>
+        <div className="exp-position-location">
+          <h3>{props.title}</h3>
+          <h4>{props.company}, {props.city}</h4>
+        </div>
+        <div className="exp-date">
+          <h4>{props.startDate} - {props.endDate}</h4>
+        </div>
+      </section>
+      <section className="content">
+        <h2 hidden="hidden">{props.title} Experience Information</h2>
+        <p>
+          {props.description}
+        </p>
+      </section>
+    </div>
+);
+
+export {Experience};
